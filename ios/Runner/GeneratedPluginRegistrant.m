59c9339fd1feea519e7882d071e1f50826ea5a68
poojaranpariya29 <148708401+poojaranpariya29@users.noreply.github.com>
2024-06-25 18:33:49 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<share_it/ShareItPlugin.h>)
+#import <share_it/ShareItPlugin.h>
+#else
+@import share_it;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ShareItPlugin registerWithRegistrar:[registry registrarForPlugin:@"ShareItPlugin"]];
+}
+
+@end
